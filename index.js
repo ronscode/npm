910e984d4cec835970d6e7a9a5a79366eda8d210
Ron Cagle <ron@atlantashirt.com>
2018-12-04 20:01:06 -0500

    Here we go

---
+++ index.js
@@ -0,0 +1,10 @@
+// const shuffle = require('lodash/shuffle');
+
+// let stripes = ["Five", "ATL", "Vamos", "AUFC"];
+
+// console.log(shuffle(stripes));
+
+var hal = require('robotjs');
+
+robot.typeString('I am a robot not a human.');
+robot.keyTap('enter');
